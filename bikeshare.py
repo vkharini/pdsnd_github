bc458f5ca5781ad9ee4e4dce66fab3cdff012eac
Harini_VK <vkharini@gmail.com>
2019-11-04 15:40:05 +0530

    docs:Included documentation for libraries used

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,9 @@
+#Load libraries
 import datetime as dt
 import time
 import pandas as pd
 import numpy as np
+
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
